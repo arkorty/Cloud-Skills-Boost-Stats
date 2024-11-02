70cd79f2f0f2d089be0d4d0150173d624e67b7c6
Arkaprabha Chakraborty <arkorty@gmail.com>
2024-11-02 08:09:16 +0530

    chore: improve open graph metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,16 @@ export const metadata: Metadata = {
   title: "BoostStats",
   description:
     "An interactive platform for managing student progress, showcasing assignments, badges, and course completion in a user-friendly interface.",
+  openGraph: {
+    title: "BoostStats",
+    description:
+      "An interactive platform for managing student progress, showcasing assignments, badges, and course completion in a user-friendly interface.",
+    url: "https://booststats.vercel.app",
+    siteName: "BoostStats",
+    images: "https://booststats.vercel.app/og-image.png",
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
